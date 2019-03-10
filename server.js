87648837a4d1ec3b44e76fbd71ee09ee9247feb7
n.bakulin <n.bakulin@xsolla.com>
2019-03-11 01:11:40 +0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+const config = require('./etc/config.json');
+const note = require('./server/models/Note');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+console.log(`Env port is ${process.env.PORT}`);
+let serverPort = process.env.PORT || 8000;
+
+const Note = mongoose.model('Note');
+const app = express();
+
+setUpConnection();
+
+app.use( bodyParser.json() );
+app.use(cors({ origin: '*' }));
+
+app.get('/notes', (req, res) => {
+    listNotes().then(data => res.send(data));
+});
+
+app.post('/notes', (req, res) => {
+    createNote(req.body).then(data => res.send(data));
+});
+
+app.delete('/notes/:id', (req, res) => {
+    deleteNote(req.params.id).then(data => res.send(data));
+});
+
+function setUpConnection() {
+    mongoose.connect(`mongodb://${config.db.userName}:${config.db.userPassword}@${config.db.host}:${config.db.port}/${config.db.name}`);
+}
+
+function listNotes(id) {
+    return Note.find(id);
+}
+
+function createNote(data) {
+    const note = new Note({
+        title: data.title,
+        text: data.text,
+        color: data.color,
+        createdAt: new Date()
+    });
+
+    return note.save();
+}
+
+function deleteNote(id) {
+    return Note.findById(id).remove();
+}
+
+const server = app.listen(serverPort, function() {
+    console.log(`Server is up and running on port ${serverPort}`);
+});
---
+++ server/config/db.js
@@ -0,0 +1,3 @@
+module.exports = {
+    url : "mongodb://NBakulin:1234567n@ds042898.mlab.com:42898/crudapidatabase"
+};
